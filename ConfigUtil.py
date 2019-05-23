646993a468aaa5f5859db2625a3da913cde41ca3
Reece Dunham <me@rdil.rocks>
2019-05-23 14:39:55 -0400

    fix?

--- ConfigUtil.py
+++ ConfigUtil.py
@@ -1,4 +1,20 @@
+"""
+    Cakebot - A cake themed Discord bot
+    Copyright (C) 2019-current year  Reece Dunham
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import json
+import FileUtil
 
 default_options = {
     'beta_features': False
@@ -15,14 +31,11 @@ def add_server(server_id, server_list):
     server_list[server_id] = default_options
 
     # Write to file
-    with open("serveropts.json", "w") as config:
+    with FileUtil.AbstractFile("/home/jumbocakeyumyum/cakebot/serveropts.json").wrap() as config:
         config.write(json.dumps({"servers": server_list}, indent=1))
         config.close()
 
 
 # reads in the config
 def get_servers():
-    with open("serveropts.json", "r") as config:
-        servers = json.load(config)
-
-    return servers["servers"]
+    return json.load(FileUtil.AbstractFile("/home/jumbocakeyumyum/cakebot/serveropts.json").wrap())["servers"]
