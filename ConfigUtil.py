597b1fbe633ec3b2ca03b06a87a5038b48d7c233
Param Thakkar <pht0104@gmail.com>
2019-05-22 22:43:43 -0400

    Fix flake8 for ConfigUtil.py

--- ConfigUtil.py
+++ ConfigUtil.py
@@ -4,19 +4,21 @@ default_options = {
     'better_config': True
 }
 
+
 # add to the server config when a bot joins a new server
 def add_server(server_id, server_list):
     # Checks for duplicates
     for s_id in server_list.keys():
         if s_id == server_id:
-            return;
-    
+            return
+
     server_list[server_id] = default_options
 
     # Write to file
     with open("serveropts.json", "w") as config:
-        config.write(json.dumps({"servers":server_list}, indent=1))
+        config.write(json.dumps({"servers": server_list}, indent=1))
         config.close()
+
 
 # reads in the config
 def get_servers():
