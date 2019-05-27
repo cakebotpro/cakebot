d7288abe00146696631c65b3e763fdff1409fb5d
Reece Dunham <me@rdil.rocks>
2019-05-26 20:16:56 -0400

    configure logging

--- LogsUtil.py
+++ LogsUtil.py
@@ -17,6 +17,6 @@ import logging
 
 logger = logging.getLogger('bot')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='/home/jumbocakeyumyum/cakebot/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
