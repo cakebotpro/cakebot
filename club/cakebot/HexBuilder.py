ddf34d98d662e0dc9eef00f249b1793653fe1038
Reece Dunham <me@rdil.rocks>
2019-05-31 09:59:36 -0400

    try this out

---
+++ club/cakebot/HexBuilder.py
@@ -0,0 +1,33 @@
+"""
+    Cakebot - A cake themed Discord bot
+    Copyright (C) 2019-current year  Reece Dunham
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import IntBuilder
+
+
+class Imp:
+    def __init__(self):
+        self.c = []
+
+    def __str__(self):
+        values = IntBuilder.random_hex()
+        for v in values:
+            self.c.append(str(v))
+
+    def __done__(self):
+        self.__str__()
+        return self.c
+
+def r():
+    return int("0x" + Imp.__done__())
