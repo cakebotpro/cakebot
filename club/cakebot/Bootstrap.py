41b77f5dce05310a79e71c70c0e41011531ffe44
Reece Dunham <me@rdil.rocks>
2019-06-03 09:13:58 -0400

    what

--- club/cakebot/Bootstrap.py
+++ club/cakebot/Bootstrap.py
@@ -13,18 +13,13 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import area4
 import discord
 
 
 def bootstrap(c, s):
-    # update servers
     for channel in c.get_all_channels():
         s.refresh()
         if channel.server.name not in s.get_cache():
             s.get_file().wrap().write(channel.server.name + "\n")
-
     await c.change_presence(game=discord.Game(name="Heya! Run +help", type=1))
-    print(area4.divider(1))
     print("Ready to roll, I'll see you on Discord: @" + c.user.__str__())
-    print(area4.divider(1))
