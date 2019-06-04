694304b1ba473bc52cc8a4c844d6e72a6555774a
Reece Dunham <me@rdil.rocks>
2019-06-04 16:33:51 -0400

    fixed stuff up with formatting

--- main.py
+++ main.py
@@ -169,9 +169,9 @@ async def on_message(message):
         btm = SlotMachineGame.arraything()
         await client.send_message(
             message.channel, ""
-            + f"  {top[0]}{top[1]}{top[2]}\n"
-            + f"> {slotz[1][0]}{slotz[1][1]}{slotz[1][2]} <\n"
-            + f"  {btm[0]}{btm[1]}{btm[2]}"
+            + f"   {top[0]}{top[1]}{top[2]}\n"
+            + f"**>** {slotz[1][0]}{slotz[1][1]}{slotz[1][2]} **<**\n"
+            + f"   {btm[0]}{btm[1]}{btm[2]}"
         )
         form = "lose"
         if slotz[0] != 0:
