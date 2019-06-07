04cbb04e0902d482afbe2ccf7776c4b805cc80b9
Reece Dunham <me@rdil.rocks>
2019-06-07 09:24:31 -0400

    strange

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ import filehandlers as fhm
 import github
 import requests
 import iss
-import datetime
 import slots
 import reverse_geocoder as rg
 from club.cakebot import EmbedUtil, ServerUtil, TextCommandsUtil, Bootstrap
@@ -48,7 +47,7 @@ client = discord.Client()
 async def on_ready():
     Bootstrap.bootstrap(client, fhm.FileHandler(fhm.AbstractFile("/home/jumbocakeyumyum/cakebot/servers.txt")))
     await client.change_presence(game=discord.Game(name="Heya! Run +help", type=1))
-    logger.info(str(datetime.datetime.now()) + ": Ready to roll, I'll see you on Discord: @" + client.user.__str__())
+    logger.info("Ready to roll, I'll see you on Discord: @" + client.user.__str__())
 
 
 # Called on message event
