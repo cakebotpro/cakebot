f028ffbfa58a1e99cb2a7745f3d2d0c90e72773a
Reece Dunham <me@rdil.rocks>
2019-06-11 11:22:36 -0400

    Apply new colors

--- club/cakebot/ColourUtil.py
+++ club/cakebot/ColourUtil.py
@@ -40,7 +40,10 @@ def random():
         Color.red(),
         Color.dark_red(),
         Color.lighter_grey(),
+        Color.light_gray(),
+        Color.dark_gray(),
         Color.darker_grey(),
+        Color.blurple(),
         tan,
         icedcoffee,
         chilipepper
