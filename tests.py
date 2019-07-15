cb3ca05e08b6b4b8141e0230b117d69521e4e09c
Unknown <me@rdil.rocks>
2019-07-15 10:00:34 -0400

    Basic Unittests I guess

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+try:
+    import slots
+except ImportError:
+    raise OSError("Failed to load dependencies")
+
+
+class Tests(unittest.TestCase):
+    def test_slots(self):
+        self.assertIsNotNone(slots.emojis)
+        self.assertIsNotNone(slots.result())
+        self.assertIsNotNone(slots.row())
+        self.assertEqual(slots.emojis, "🍎🍊🍐🍋🍉🍇🍒")
