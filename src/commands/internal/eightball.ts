d9cdce9dcb58b86a874712fd7fa5369c7f7e8382
Reece Dunham <me@rdil.rocks>
2021-01-15 16:38:00 -0500

    Spacing fix

--- src/commands/internal/eightball.ts
+++ src/commands/internal/eightball.ts
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 import random from "random"
 import { eightballs } from "../../data/remote/runtime-data"
 import Command from "../commands"
